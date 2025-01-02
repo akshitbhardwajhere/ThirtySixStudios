8fd415ec2794582199a14a6c53c8732ebdc1b9e8
Akshit Bhardwaj <akshitbhardwaj257448@gmail.com>
2025-01-02 12:14:27 +0530

    All done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,187 @@
 import './index.css'
 import Canvas from './Canvas'
+import data from './data'
+import { useEffect, useState, useRef } from 'react';
+import LocomotiveScroll from 'locomotive-scroll';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 function App() {
-  return (
-    <div className="w-full min-h-screen bg-black text-white">
-      <Canvas/>
-    </div>
+  const [showCanvas, setShowCanvas] = useState(false);
+  const showCanvasRef = useRef(null);
+  const growSpanref = useRef(null);
+
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, [])
+  
+  useEffect(() => {
+    const handleClick = (e) => {
+      setShowCanvas((prevShowCanvas) => {
+        if (!prevShowCanvas) {
+          gsap.set(growSpanref.current, {
+            top: e.clientY,
+            left: e.clientX,
+          })
+  
+          gsap.to('body', {
+            color: "#000", 
+            backgroundColor: "#fd2c2a",
+            duration: 1,
+            ease: 'power2.inOut',
+          })
+          
+          gsap.to(growSpanref.current, {
+            scale: 10000,
+            duration: 2,
+            ease: 'power2.inOut',
+            onComplete: () => {
+              gsap.set(growSpanref.current, {
+                scale: 0,
+                clearProps: 'all',
+              })
+            }
+          })
+        }
+        else {
+          gsap.to('body', {
+            color: "#fff",
+            backgroundColor: "#000",
+            duration: 1.2,
+            ease: 'power2.inOut',
+          })
+        }
+        return !prevShowCanvas;
+      });
+    }
+
+    const heading = showCanvasRef.current;
+    heading.addEventListener('click', handleClick);
+
+    return () => heading.removeEventListener('click', handleClick);
+    
+  }, [])
+
+  return(
+    <>
+      <div className='relative w-full min-h-screen   font-["Segoe_UI"]'>
+
+        <span ref={growSpanref} className='bg-[#fd2c2a] grow block fixed top-[-20px] left-[-20px] w-2 h-2 rounded-full'></span>
+
+        {showCanvas && data[0].map((canvasDetails, index) => {
+          return <Canvas details={canvasDetails} />
+        })}
+
+        <div className='w-full h-screen relative'>
+
+          <nav className='w-full p-2  flex justify-between items-center border-b border-gray-400'>
+            <div className='brand   text-2xl font-regular'>
+              <h1>thirtysixstudio</h1>
+            </div>
+            <div className='links flex gap-10'>
+              {["What we do", "Who we are", "How we give back", "Talk to us"].map((link, index) => (
+                <a key={index} href={`#${link.toLowerCase()}`}
+                className='text-medium hover:text-gray-300'>{link}</a>
+              ))}
+            </div>
+          </nav>
+
+          <div className='textcontainer mt-2 px-[20%] w-full'>
+
+            <div className='text w-[50%]'>
+
+              <h3 className='text-3xl leading-[1.5]'>
+                At Thirtysixstudio, we build immersive digital experiences for brands with a purpose.
+              </h3>
+
+              <p className='text-lg w-[80%] mt-10 font-thin leading-[1.5]'>
+                We’re a boutique production studio focused on design, motion, and creative technology, constantly reimagining what digital craft can do for present-time ads and campaigns.
+              </p>
+
+              <p className='text-medium w-[80%] mt-10 font-md leading-[1.5]'>
+                Scroll
+              </p>
+
+            </div>
+
+          </div>
+
+          <div className='flex justify-center items-center absolute bottom-0 left-0 w-full'>
+
+            <h1 ref={showCanvasRef} className='text-[15.8rem] font-normal tracking-tight leading-none'>
+              Thirtysixstudio
+            </h1>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='w-full relative h-screen  mt-10 px-10'>
+
+      {showCanvas && data[1].map((canvasDetails, index) => {
+          return <Canvas details={canvasDetails} />
+        })}
+
+        <h1 className='text-5xl'>About the studio</h1>
+        
+        <p className='text-2xl w-[80%] mt-10 font-thin pl-10 leading-[1.8]'>We provide captivating design, interactive animations, advanced usability, reliable code, and immaculate project coordination. Whether you need a campaign built from scratch or assistance at a specific phase, we’ve got you covered.</p>
+
+      </div>
+
+
+
+      {/* You can add content in the divs below */}
+
+      {/* <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[2].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div>
+
+      <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[3].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div>
+
+      <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[4].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div>
+
+      <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[5].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div>
+
+      <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[6].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div>
+
+      <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[7].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div>
+
+      <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[8].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div>
+
+      <div className='relative w-full min-h-screen  '>
+        {showCanvas && data[9].map((canvasDetails, index) => {
+            return <Canvas details={canvasDetails} />
+           })}
+      </div> */}
+    </>
   )
 }
 
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,27 +1,66 @@
 import React, { useEffect, useRef, useState } from 'react';
 import canvasImages from './canvas-images';
-import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
-const Canvas = () => {
-    const [index, setIndex] = useState({ value:0 });
+const Canvas = ({ details }) => {
+    const { startIndex, numImages, duration, size, top, left, zIndex } = details;
+    const [index, setIndex] = useState({ value: startIndex });
     const canvasRef = useRef(null);
 
+    useGSAP(() => {
+        gsap.to(index, {
+            value: startIndex + numImages - 1,
+            duration: duration,
+            repeat: -1,
+            ease: 'linear',
+            onUpdate: () => {
+                setIndex({ value: Math.round(index.value) });
+            }
+        })
+        gsap.from(canvasRef.current, {
+            opacity: 0,
+            duration: 1,
+            ease: 'power2.inOut',
+            delay: 0.1
+        })
+    })
+
     useEffect(() => {
+        const scale = window.devicePixelRatio;
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         
-        const image = new Image();
-        image.src = canvasImages[index.value];
+        const img = new Image();
+        img.src = canvasImages[index.value];
         
-        image.onload = () => {
-            canvas.width = image.width;
-            canvas.height = image.height;
-            ctx.drawImage(image, 0, 0);
+        img.onload = () => {
+            canvas.width = canvas.offsetWidth * scale;
+            canvas.height = canvas.offsetHeight * scale;
+            canvas.style.width = canvas.offsetWidth + 'px';
+            canvas.style.height = canvas.offsetHeight + 'px';
+
+            ctx.scale(scale, scale);
+            ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
         };
-    });
+    }, [index]);
         
-    return <canvas ref={canvasRef} className='w-[18rem] h-[18rem]' id="canvas"></canvas>
+
+    return (
+        <canvas 
+        data-scroll
+        data-scroll-speed={Math.random().toFixed(1)} 
+        ref={canvasRef} 
+        className='absolute' 
+        style={{ 
+            width: `${size * 1.4}px`, 
+            height: `${size * 1.4}px`, 
+            top: `${top}%`, 
+            left: `${left}%`, 
+            zIndex: `${zIndex}` 
+        }} 
+        id="canvas"></canvas>
+    )
 }
 
 export default Canvas;
